cdbec8fa65c75a800c7b1ce361f6ecd0b172f890
Thomas <ixam.agem@gmail.com>
2024-02-26 15:59:23 +0100

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "QA-Blog",
+    base: "/QA-Blog/",
     plugins: [vue()],
     build: {
         minify: true,
