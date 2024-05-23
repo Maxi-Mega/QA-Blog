de52c62b36fd5a56f0fbc831460fd409ca706f39
Thomas <ixam.agem@gmail.com>
2024-05-23 09:32:21 +0200

    Improve login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,33 @@
 <script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
 const baseUrl = import.meta.env.BASE_URL;
+const capsLock = ref(false);
+const showPasswd = ref(false);
+
+onMounted(() => {
+  const passwdInput = document.getElementById("password");
+  if (!passwdInput) {
+    throw new Error("Password input not found");
+  }
+
+  passwdInput.addEventListener("keyup", (ev) => {
+    if (ev.key === "CapsLock") {
+      capsLock.value = !capsLock.value;
+    } else {
+      capsLock.value = ev.getModifierState("CapsLock");
+    }
+  });
+
+  const showPasswdInput = document.getElementById("showPasswd");
+  if (!showPasswdInput) {
+    throw new Error("Password show input not found");
+  }
+
+  showPasswdInput.addEventListener("input", (ev) => {
+    showPasswd.value = ev.target.checked;
+  });
+});
 </script>
 
 <template>
@@ -28,18 +56,27 @@ const baseUrl = import.meta.env.BASE_URL;
           </div>
           <div class="w-full">
             <label class="mb-1 flex flex-wrap gap-1" for="password">Password</label>
-            <input
-              id="password"
-              autocomplete="current-password"
-              class="border-1 w-full rounded border-gray-300 bg-gray-800 p-1 text-gray-100"
-              minlength="8"
-              name="password"
-              placeholder=""
-              required
-              type="password"
-            />
+            <div class="inline w-full">
+              <input
+                id="password"
+                autocomplete="current-password"
+                class="border-1 w-11/12 flex-grow rounded border-gray-300 bg-gray-800 p-1 text-gray-100"
+                minlength="8"
+                name="password"
+                placeholder=""
+                required
+                :type="showPasswd ? 'text' : 'password'"
+              />
+              <input id="showPasswd" type="checkbox" title="Show password" class="ml-2" />
+            </div>
+            <p
+              :class="'mt-1 w-1/12 ' + (capsLock ? 'text-red-700' : 'text-transparent')"
+              :aria-hidden="!capsLock"
+            >
+              caps lock is on
+            </p>
           </div>
-          <div class="my-3 w-full border-t-2 border-gray-500"></div>
+          <div class="mb-3 w-full border-t-2 border-gray-500"></div>
           <div class="w-full">
             <input
               class="w-full cursor-pointer rounded border-2 border-gray-200 px-2 py-1 transition-all hover:rounded-xl hover:bg-gray-100 hover:bg-opacity-5"
