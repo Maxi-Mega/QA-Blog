39db74a02958beb3f860c2e93f70123b81d32d4a
Thomas <ixam.agem@gmail.com>
2024-06-13 09:23:32 +0200

    Add popup for successful login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <script lang="ts" setup>
 import { useArticles } from "../composables/useArticles.ts";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import ArticlePreview from "../components/ArticlePreview.vue";
 
 const articles = ref(useArticles());
+const loginSuccessful = ref(false);
+
+onMounted(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has("username") && urlParams.has("password")) {
+    loginSuccessful.value = true;
+  }
+});
 </script>
 
 <template>
   <div class="py-6 sm:py-8 lg:py-12">
+    <span
+      v-if="loginSuccessful"
+      id="login-success"
+      class="absolute left-1/2 top-0 mt-2 -translate-x-1/2 rounded-md bg-lime-300 bg-opacity-50 p-2"
+      >Logged in successfully</span
+    >
     <div class="mx-auto max-w-screen-xl px-4 md:px-8">
       <section>
         <h1 class="mb-2 text-xl">Latest articles</h1>
@@ -19,4 +33,18 @@ const articles = ref(useArticles());
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#login-success {
+  opacity: 1;
+  animation: fade-out 0.5s 2s ease-out forwards;
+}
+
+@keyframes fade-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
